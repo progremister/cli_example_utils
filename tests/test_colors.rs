0c590c4152075708704958904de198345d1f9211
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2024-03-10 12:59:57 +0100

    Added a test of colors module

---
+++ tests/test_colors.rs
@@ -0,0 +1,13 @@
+use cli_example_utils::colors::{ColorString, Color};
+
+#[test]
+fn test_red_coloring() {
+    let mut color_string = ColorString {
+        color: Color::Red,
+        string: "Red".to_string(),
+        colorized: "".to_string()
+    };
+
+    color_string.paint();
+    assert_eq!(color_string.colorized, "\x1b[31m{}\x1b[0m");
+}
