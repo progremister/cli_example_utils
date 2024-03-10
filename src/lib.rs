b4949cfec5a76c5977941cf39571c321edde83d7
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2024-03-10 10:49:16 +0100

    Documented the module

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,22 @@
+//! This is a library that provides utilities for command-line tools.
+//! So far it only provides a function to read a line from stdin.
+//! # Examples:
+//! ```
+//! use cli_example_utils::read_stdin;
+//! let input = read_stdin();
+//! ```
+//!
+//! # Panics:
+//! The `read_stdin` function will panic if it fails to read a line with a message "Failed to read input line!"
+
 use std::io::{BufReader, BufRead};
+
+/// This function reads a line from stdin and returns it as a String
+/// It will panic if it fails to read a line with a message "Failed to read input line!"
+/// # Examples:
+/// ```
+/// let input = read_stdin();
+/// ```
 
 pub fn read_stdin() -> String {
     let stdin = std::io::stdin();
