4e80961fc097996237ff70db288bb13e09f212d0
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2024-03-10 13:12:57 +0100

    Added tests of the private function in the colors module

--- src/lib.rs
+++ src/lib.rs
@@ -24,9 +24,35 @@ pub mod config;
 pub fn read_stdin() -> String {
     let stdin = std::io::stdin();
     let mut reader = BufReader::new(stdin.lock());
+    _read_stdin(&mut reader)
+
+}
+
+fn _read_stdin<T: BufRead>(reader: &mut T) -> String {
     let mut line = String::new();
     reader
         .read_line(&mut line)
         .expect("Failed to read input line!");
     line.trim().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::_read_stdin;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_read_stdin() {
+        let mut reader = Cursor::new("Test\n");
+        let line = _read_stdin(&mut reader);
+        assert_eq!(line, "Test")
+    }
+
+    #[test]
+    fn test_read_stdin_empty() {
+        let mut reader = Cursor::new("\n");
+        let line = _read_stdin(&mut reader);
+        assert_eq!(line, "")
+
+    }
 }
